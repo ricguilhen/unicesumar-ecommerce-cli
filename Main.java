7b91e990c0b96befe45777b8685d2f05163eef63
Rafael Labegalini <rjlabegalini8@gmail.com>
2025-03-19 19:47:55 -0300

    Criando menu no terminal

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
+
 import entities.Product;
 import repository.InMemoryRepository;
 
 public class Main {
 
     public static void main(String[] args) {
-        InMemoryRepository<Product> productRepository = new InMemoryRepository<>();
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
-        productRepository.save(new Product("Impressora", 100));
-        productRepository.save(new Product("Computador", 3000));
+        do {
+            System.out.println("\033[H\033[2J");
+            System.out.flush();
 
-        System.out.println("Exibindo produtos");
-        productRepository.findAll().forEach(item -> System.out.println(item.getName()));
+            System.out.println("\n---MENU---");
+            System.out.println("1 - Cadastrar Produto");
+            System.out.println("2 - Buscar Produto");
+            System.out.println("3 - Sair");
+            System.out.println("Escolha uma opção: ");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Cadastrar Produto");
+                    break;
+                case 2:
+                    System.out.println("Buscar Produto");
+                    break;
+                case 3:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente");;
+            }
+
+        } while (option != 3);
+
+        scanner.close();
     }
 }
