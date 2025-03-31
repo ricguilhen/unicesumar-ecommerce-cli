7d01b87c1477bbc08e75c31f72daac0aa27d7561
Rafael Labegalini <48809390+rafael-labegalini@users.noreply.github.com>
2025-03-31 17:40:36 -0300

    Adicionando script SQL para tabela de usuários

--- scripts/init.sql
+++ scripts/init.sql
@@ -3,3 +3,10 @@ CREATE TABLE products (
     name VARCHAR(255) NOT NULL,  -- Nome do produto
     price NUMERIC(10, 2) NOT NULL -- Preço do produto com até 10 dígitos e 2 casas decimais
 );
+
+CREATE TABLE users (
+    uuid UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
