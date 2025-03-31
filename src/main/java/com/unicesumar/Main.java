3da614fe4b7a8364b3aabdc72c323fc1c569650e
Rafael Labegalini <48809390+rafael-labegalini@users.noreply.github.com>
2025-03-31 17:18:49 -0300

    Adicionando novas opções ao Menu

--- src/main/java/com/unicesumar/Main.java
+++ src/main/java/com/unicesumar/Main.java
@@ -7,9 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
@@ -39,8 +37,10 @@ public class Main {
         do {
             System.out.println("\n---MENU---");
             System.out.println("1 - Cadastrar Produto");
-            System.out.println("2 - Buscar Produto");
-            System.out.println("3 - Sair");
+            System.out.println("2 - Listas Produtos");
+            System.out.println("3 - Cadastrar Usuário");
+            System.out.println("4 - Listar Usuários");
+            System.out.println("5 - Sair");
             System.out.println("Escolha uma opção: ");
             option = scanner.nextInt();
 
@@ -51,11 +51,17 @@ public class Main {
                     listaDeProdutos.save(new Product("Computador", 3000));
                     break;
                 case 2:
-                    System.out.println("Buscar Produto");
+                    System.out.println("Listar Produtos");
                     List<Product> products = listaDeProdutos.findAll();
                     products.forEach(System.out::println);
                     break;
                 case 3:
+                    System.out.println("Cadastrar Usuário");
+                    break;
+                case 4:
+                    System.out.println("Listar Usuários");
+                    break;
+                case 5:
                     System.out.println("Saindo...");
                     break;
                 default:
